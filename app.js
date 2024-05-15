988c590ab0e9ade3bd13a915300c1fe1ce4c1fdd
AEPALO00 <palomaresv.ae@gmail.com>
2024-05-14 22:17:42 -0400

    first commit

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const SECRET_KEY = 'your_secret_key';
+const SECRET_KEY = 'aepalo00^#';
 
 mongoose.set('strictQuery', false);
 
-const uri =  "mongodb://root:<replace password>@localhost:27017";
+const uri =  "mongodb://root:MjExMDUtYTAxNTcw@localhost:27017";
 mongoose.connect(uri,{'dbName':'SocialDB'});
 
-const User = mongoose.model('User', { username: String, email: String, password: String });
-const Post = mongoose.model('Post', { userId: mongoose.Schema.Types.ObjectId, text: String });
+const User = mongoose.model('User', { 
+    username: String, 
+    email: String, 
+    password: String });
+const Post = mongoose.model('Post', { 
+    userId: mongoose.Schema.Types.ObjectId, 
+    text: String });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,14 +27,81 @@ app.use(session({ secret: SECRET_KEY, resave: false, saveUninitialized: true, co
 
 
 // Insert your authenticateJWT Function code here.
+function authenticateJWT(req, res, next) {
+    const token = req.session.token;
+
+    if (!token) return res.status(401).json({message:"Unauthorized"});
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+}
 
 // Insert your requireAuth Function code here.
+function requireAuth(req, res, next) {
+    const token = req.session.token;
+
+    if(!token) return res.redirect("/login");
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.redirect("/login");
+    }
+}
 
 // Insert your routing HTML code here.
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'public', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'public', 'login.html')));
+app.get('/post', requireAuth, (req, res) => res.sendFile(path.join(__dirname, 'public', 'post.html')));
+app.get('/index', requireAuth, (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html'), { username: req.user.username }));
 
 // Insert your user registration code here.
+app.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+
+        if (existingUser) return res.status(400).json({ message: 'User already exists' });
+
+        const newUser = new User({ username, email, password });
+        await newUser.save();
+
+        const token = jwt.sign({ userId: newUser._id, username: newUser.username }, SECRET_KEY, { expiresIn: '1h' });
+        req.session.token = token;
+        //res.send({"message":`The user ${username}has been added`});
+        res.redirect("/index")
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Internal Server Error"})
+    }
+})
 
 // Insert your user login code here.
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ username, password });
+
+        if (!existingUser) return res.status(400).json({ message: 'Invalid Credentials' });
+
+        const token = jwt.sign({ userId: newUser._id, username: newUser.username }, SECRET_KEY, { expiresIn: '1h' });
+        req.session.token = token;
+        res.send({"message":`The user ${username}has been added`});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Internal Server Error"})
+    }
+})
 
 // Insert your post creation code here.
 
